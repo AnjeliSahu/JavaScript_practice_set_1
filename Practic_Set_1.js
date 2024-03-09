6ea30feb1fd236c25f9f9cc1bf92d62ffcf38af4
Anjeli Sahu <144547951+AnjeliSahu@users.noreply.github.com>
2024-03-09 17:19:57 +0530

    Add files via upload

---
+++ Practic_Set_1.js
@@ -0,0 +1,30 @@
+// Q1: Creat a variable of type string and try to add a number to it
+let a ="Anjeli";
+let b= 67;
+let c=a+b;
+console.log(c)
+
+// Q2: use typeof oprator to find the datatype of the string in above qus
+console.log(typeof (a+b))
+
+//Q3: create a const object in javascript can you change it to hold a number later
+const n  = {
+     name : "Anjeli",
+     section: 1,
+     isstudent:true 
+}
+
+//Q4: Add a new key
+n['name']="Disha";
+console.log(n)
+
+//Q5:Write a js program to creat a ward memoring dictionary of 5 words
+const dict = {
+    appreciate: "recognize the full worth of .",
+    ataraxia:"a state of restlessness.",
+    gnomon:"a creature of the night",
+    yakka:"work hard",
+    tenor:"a musical voice"
+
+}
+console.log(dict.yakka)
